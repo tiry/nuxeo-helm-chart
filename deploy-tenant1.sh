40b16bcf269d549d901924753fc3f86ffe106ef7
Thierry Delprat <tdelprat@nuxeo.com>
2020-11-11 13:26:45 -0500

    TL-366 session affinity + scale out

--- deploy-tenant1.sh
+++ deploy-tenant1.sh
@@ -3,7 +3,7 @@
 echo "Deploy tenant1"
 
 #kubectl create namespace company-a
-helm3 upgrade -i nuxeo \
+helm3 upgrade -i nuxeo-t1 \
 	 -f nuxeo/tenant1-values.yaml \
 	 --debug \
 	 --set nuxeo.clid=${NXCLID} \
---
+++ scale-tenant.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+kubectl scale deployment/nuxeo-t1-nuxeo-company-a --replicas=2 -n company-a
+
