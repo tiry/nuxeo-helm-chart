6402dacff698a07979bc1b0dfb3288908e576d9a
Thierry Delprat <tdelprat@nuxeo.com>
2020-12-02 16:20:28 -0500

    TL-366 ajust structure to make merge easier

--- deploy-tenant1.sh
+++ deploy-tenant1.sh
@@ -4,8 +4,8 @@ echo "Deploy tenant1"
 
 helm3 upgrade -i nuxeo \
      -n tenant1 --create-namespace \
-	 -f nuxeo/nuxeo-shared-values.yaml \
-	 -f nuxeo/tenant1-values.yaml \
+	 -f tenants/nuxeo-shared-values.yaml \
+	 -f tenants/tenant1-values.yaml \
 	 --debug \
 	 --set clid=${NXCLID} \
      --set gcpb64=${NXGCPB64} \
